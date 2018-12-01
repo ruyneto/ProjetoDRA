24be41211a58116d0b9b84d3b4b98f9c4b9faa9c
Izaltino <izaltino_neto@hotmail.com>
2018-12-01 01:13:10 -0400

    pastas back-end e front-end

---
+++ back-end/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var corsOptions = {
+  origin: 'http://localhost:4200',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.use(cors(corsOptions))
+app.use(bodyParser.json());
+app.get('/',function(req,res){
+res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+res.json({"lista":[{"nome":"ruyneto","senha":"321987"},
+          {"nome":"Izaltino","senha":"321987"},
+          {"nome":"Sandro","senha":"321987"},
+          {"nome":"Gilmar","senha":"321987"},
+          {"nome":"Hermani","senha":"321987"},
+          {"nome":"Bruno","senha":"321987"}
+        ]})
+});
+
+app.listen(300,function(){
+  console.log("Server on.");
+});
