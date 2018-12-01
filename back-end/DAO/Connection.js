83b633d16fefedb10917aac1053fa0788b7b6de6
ruyneto <neto3333@gmail.com>
2018-12-01 04:17:49 -0400

    Adicionar função de inserir através de post.

---
+++ back-end/DAO/Connection.js
@@ -0,0 +1,22 @@
+const MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
+var Server = require('mongodb').Server;
+var mongoclient = new MongoClient(new Server("localhost", 27017), {native_parser: true});
+var dbo;
+
+
+
+
+
+module.exports = function inserir(objeto){
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("projeto");
+    var myobj = objeto;
+    dbo.collection("usuario").insertOne(myobj, function(err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    });
+  });
+}
